7fe49714118c6c3b2c449febad9bec79dc047694
dimaslz <me@dimaslz.io>
2021-06-16 18:47:15 +0200

    fix(env): fix purge css with @craco to overwrite postcss config.

--- playground/tailwind.config.js
+++ playground/tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  purge: process.env.NODE_ENV === 'production'
-    ? [
-      'src/**/*.js',
-      'src/**/*.scss'
-    ] : [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./src/**/*.{js,jsx,ts,tsx,scss}', './public/index.html']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
